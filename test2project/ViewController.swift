5fc2048188450bcffca5da3cf260885e1806f3b1
sabano3so2 <sabano3so2@gmail.com>
2020-08-15 23:58:55 +0900

    Initial Commit

---
+++ test2project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test2project
+//
+//  Created by Masayuki WATANABE on 2020/08/15.
+//  Copyright © 2020 Masayuki WATANABE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
