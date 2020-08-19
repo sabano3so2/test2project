e606f8f8de67b6d0b01582017b83ac57b3fc09de
sabano3so2 <sabano3so2@gmail.com>
2020-08-19 12:11:51 +0900

    pod を試した

--- test2project/ViewController.swift
+++ test2project/ViewController.swift
@@ -7,12 +7,18 @@
 //　これ反映されるか　8/16!
 
 import UIKit
+//import SwiftFontName
+import SwiftFontName
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var changeFont: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        changeFont.font = UIFont.init(name: FontName.MenloItalic, size: 20)
+        
     }
 //8/17 コミットのこと確認用の変更！
     
